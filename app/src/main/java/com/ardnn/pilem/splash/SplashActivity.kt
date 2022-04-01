6a6fcce54a8291c8aa403b0666de27fd2fa418d9
muammarahlnn <21muammarahlan@gmail.com>
2022-04-01 15:24:52 +0800

    add resources and splash screen

---
+++ app/src/main/java/com/ardnn/pilem/splash/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.ardnn.pilem.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.ardnn.pilem.MainActivity
+import com.ardnn.pilem.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // set timer for 1 second
+        Handler(Looper.getMainLooper()).postDelayed({
+            // to main activity
+            val toMain = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(toMain)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            finish()
+        }, 1000)
+    }
+}
