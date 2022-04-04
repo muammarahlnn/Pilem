df26a73878b00f7548898075ee18af1b36236021
muammarahlnn <21muammarahlan@gmail.com>
2022-04-04 17:58:26 +0800

    implement encryption to room database

--- core/src/main/java/com/ardnn/pilem/core/di/DatabaseModule.kt
+++ core/src/main/java/com/ardnn/pilem/core/di/DatabaseModule.kt
@@ -2,6 +2,7 @@ package com.ardnn.pilem.core.di
 
 import android.content.Context
 import androidx.room.Room
+import com.ardnn.pilem.core.BuildConfig
 import com.ardnn.pilem.core.data.source.local.room.PilemDao
 import com.ardnn.pilem.core.data.source.local.room.PilemDatabase
 import dagger.Module
@@ -9,6 +10,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,12 +20,18 @@ class DatabaseModule {
 
     @Singleton
     @Provides
-    fun provideDatabase(@ApplicationContext context: Context): PilemDatabase =
-        Room.databaseBuilder(
+    fun provideDatabase(@ApplicationContext context: Context): PilemDatabase {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes(BuildConfig.PASSWORD_DB.toCharArray())
+        val factory = SupportFactory(passphrase)
+
+        return Room.databaseBuilder(
             context,
             PilemDatabase::class.java,
             "Pilem.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
+    }
 
     @Provides
     fun providePilemDao(database: PilemDatabase): PilemDao =
