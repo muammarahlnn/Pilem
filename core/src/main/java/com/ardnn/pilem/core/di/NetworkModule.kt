2da90c2f13a00959f383254e040a55674b6dd606
muammarahlnn <21muammarahlan@gmail.com>
2022-04-04 17:57:54 +0800

    fix RxJava retrofit adapter

--- core/src/main/java/com/ardnn/pilem/core/di/NetworkModule.kt
+++ core/src/main/java/com/ardnn/pilem/core/di/NetworkModule.kt
@@ -9,6 +9,7 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
@@ -30,6 +31,7 @@ class NetworkModule {
         val retrofit = Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL_TMDB)
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .client(client)
             .build()
         return  retrofit.create(ApiService::class.java)
